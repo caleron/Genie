3c30812e33e9af964a76281d08c8873a7faa74ed
caleron <paddy.buchhardt@arcor.de>
2016-04-25 11:37:18 +0200

    MarqueeTextView mit Animation, was auch nicht funktioniert

--- app/src/main/java/de/teyzer/genie/ui/custom/MarqueeTextView.java
+++ app/src/main/java/de/teyzer/genie/ui/custom/MarqueeTextView.java
@@ -3,6 +3,8 @@ package de.teyzer.genie.ui.custom;
 import android.content.Context;
 import android.graphics.Rect;
 import android.util.AttributeSet;
+import android.view.animation.Animation;
+import android.view.animation.TranslateAnimation;
 import android.widget.TextView;
 
 /**
@@ -11,6 +13,8 @@ import android.widget.TextView;
  * http://stackoverflow.com/questions/1827751/is-there-a-way-to-make-ellipsize-marquee-always-scroll
  */
 public class MarqueeTextView extends TextView {
+
+    private Animation marqueeAnimation;
 
     public MarqueeTextView(Context context) {
         super(context);
@@ -39,5 +43,29 @@ public class MarqueeTextView extends TextView {
     @Override
     public boolean isFocused() {
         return true;
+    }
+
+    @Override
+    public void setText(CharSequence text, BufferType type) {
+        super.setText(text, type);
+    }
+
+    public void animateTextView() {
+        int textWidth = getMeasuredWidth();
+        int width = getWidth();
+
+    /* Start animation only when text is longer than dislay width. */
+        if (width < textWidth) {
+            if (marqueeAnimation != null) {
+                marqueeAnimation.cancel();
+            }
+            marqueeAnimation = new TranslateAnimation(0, width - textWidth, 0, 0);
+            marqueeAnimation.setDuration(3000);    // Set custom duration.
+            marqueeAnimation.setStartOffset(500);    // Set custom offset.
+            marqueeAnimation.setRepeatMode(Animation.REVERSE);    // This will animate text back ater it reaches end.
+            marqueeAnimation.setRepeatCount(Animation.INFINITE);    // Infinite animation.
+
+            startAnimation(marqueeAnimation);
+        }
     }
 }
